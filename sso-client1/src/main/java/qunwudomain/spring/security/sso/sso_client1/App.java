07dff6ae880edea2711424746c4ca53b3dc15b5d
QunWu <wuqun597090371@hotmail.com>
2019-06-20 00:20:17 -0700

    sso completed

--- sso-client1/src/main/java/qunwudomain/spring/security/sso/sso_client1/App.java
+++ sso-client1/src/main/java/qunwudomain/spring/security/sso/sso_client1/App.java
@@ -3,15 +3,18 @@ package qunwudomain.spring.security.sso.sso_client1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Hello world!
- *
- */
 @SpringBootApplication
+@RestController
+@EnableOAuth2Sso
 public class App 
 {
     public static void main( String[] args )
     {
     	SpringApplication.run(App.class, args);
     }
+    
+    @GetMapping("/user")
+	public Authentication user(Authentication user) {
+		return user;
+	}
 }
