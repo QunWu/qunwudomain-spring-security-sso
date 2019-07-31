504afbd4fbe0370aba73b38d26d35addd28b0fde
Aaron Wu <aawu@aawu-mn2.linkedin.biz>
2019-07-30 17:48:38 -0700

    correct package name issue

--- sso-client1/src/main/java/qunwudomain/spring/security/sso/sso_client1/App.java
+++ sso-client1/src/main/java/qunwudomain/spring/security/sso/sso_client1/App.java
@@ -2,6 +2,10 @@ package qunwudomain.spring.security.sso.sso_client1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
